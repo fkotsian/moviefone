32e7e06fecb4bc96b1222ed8f3ee735b4e8e408d
Frank Kotsianas <frankkotsianas@gmail.com>
2020-05-10 00:39:34 -0400

    proxy api requests to 8080 to allow local dev work
    - proxy unmatched requests from dev server (:3000) to API server (:8080)
    - also separate server and routes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
+// load env
 require('dotenv').config()
 
 // server setup
 const express = require('express')
 const app = express()
-const port = 3000
 
 const movieService = require('server/movie_service')
 
@@ -11,6 +11,9 @@ app.get('/', (req, res) => res.send('loads built React'))
 
 app.get('/popular', (req, res) => {
 
+// serve static assets
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(port, () => console.log(`Listening on port :${port}!`))
+module.exports = app
